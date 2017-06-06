b9636f6db4f2db44c6d8d6404d705c2b9a1e47cc
Edward Stevinson <edward.stevinson@hotmail.com>
2017-06-06 16:14:24 +0100

    Changed source_score to a float

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170606143108) do
+ActiveRecord::Schema.define(version: 20170606150814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,10 +48,10 @@ ActiveRecord::Schema.define(version: 20170606143108) do
   create_table "sources", force: :cascade do |t|
     t.string   "name"
     t.string   "url_base"
-    t.integer  "source_score"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.integer  "link_id"
+    t.float    "source_score"
     t.index ["link_id"], name: "index_sources_on_link_id", using: :btree
   end
 
