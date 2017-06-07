8a1f31f18d4651df49bca1fbc576da94081fa597
Amal Ketata <amal.ketata@edu.escpeurope.eu>
2017-06-07 17:00:33 +0100

    Links controller amended, added the case where the urls array is empty

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -16,13 +16,15 @@ class LinksController < ApplicationController
     user = User.find_by(uid: id)
     # Get access to all the sources
     sources = Source.all
-    # Iterate over each of the urls
-    urls.each do |url|
-      # Check the url against each of the sources 'url_base'
-      sources.each do |source|
-        # If it matches then create a Links table entry with reference to the source and user matched
-        if url.include? source.url_base
-          Link.create!(url: url, user: user, source: source)
+    # Iterate over each of the urls if links received
+    if urls
+      urls.each do |url|
+        # Check the url against each of the sources 'url_base'
+        sources.each do |source|
+          # If it matches then create a Links table entry with reference to the source and user matched
+          if url.include? source.url_base
+            Link.create!(url: url, user: user, source: source)
+          end
         end
       end
     end
