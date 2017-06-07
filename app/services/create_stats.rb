ef0911bd12468c040ac675b04fd5ab997df57399
Edward Stevinson <edward.stevinson@hotmail.com>
2017-06-07 11:30:44 +0100

    Created a service object to create stats

---
+++ app/services/create_stats.rb
@@ -0,0 +1,20 @@
+# Service object that is run every X hours to perform the business logic that
+# creates the JSONb files that are given to a user to display the data collected
+# by the extension
+class CreateStats
+
+  # Constructor
+  # Accept input
+  def initialise(params)
+    # Class instances with @sign
+  end
+
+  # Perform work
+
+  # Different methods for each of the JSONb's
+
+  # Return result
+
+  private
+
+end
