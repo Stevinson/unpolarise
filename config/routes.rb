6e820d976e294488ce92fae9ede5959a02ccc2f0
Sandrine Ayral <sandrine.ayral@gmail.com>
2017-06-09 18:43:24 +0100

    changed routes for chrome extension purposes

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,9 @@ Rails.application.routes.draw do
   resources :articles, only: [:show] # All the other actions are managed through Rails Admin (index, new, create, update, delete)
   get 'suggested_articles', to: 'articles#index', as: 'suggested_articles'
 
+  resources :users, only: [:show]
+
+  #Logged out Home Page
   root to: 'pages#home'
 
   # The links controller needs to have an action that receives the post requests from the extension
