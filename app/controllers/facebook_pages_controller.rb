08f7d4a1b73dcc7d5604793dd24f0fb35cbb7c34
Edward Stevinson <edward.stevinson@hotmail.com>
2017-06-12 20:11:36 +0100

    Added likes column to user model

--- app/controllers/facebook_pages_controller.rb
+++ app/controllers/facebook_pages_controller.rb
@@ -10,24 +10,14 @@ class FacebookPagesController < ApplicationController
 
   def create
     # Get the user ID and links that have been sent from the extension
-    id = params[:id] # received as a string [to be checked]
-    urls = params[:urls] # received as an array
-    urls = urls.gsub(/[\[\]]/, '').split(', ') # Just for testing with Postman
+    name = params[:name] # String which is the user's name
+    likes_array = params[:likes_array] # Receives an array of hashes
     # Find the corresponding user
-    user = User.find_by(id: id)
-    # Get access to all the sources
-    sources = Source.all
-    # Iterate over each of the urls if links received
-    if urls
-      urls.each do |url|
-        # Check the url against each of the sources 'url_base'
-        sources.each do |source|
-          # If it matches then create a Links table entry with reference to the source and user matched
-          if url.include? source.url_base
-            Link.create!(url: url, user: user, source: source)
-          end
-        end
-      end
+    user = User.find_by(first_name: name)
+    # Iterate over each of the hashes if received something
+    if likes_array
+      user.like_data = likes_array # Needs to be a hash!!!!!!!!!!!!!!!!!!!
+      user.save
     end
   end
 end
