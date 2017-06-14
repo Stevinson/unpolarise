2110c775b9a2ed3db2ca800dd83f746bdf3f19fb
Amal Ketata <amal.ketata@edu.escpeurope.eu>
2017-06-14 12:31:29 +0100

    Work in progress. Create to be implemented with JS

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,4 +1,17 @@
 class BookmarksController < ApplicationController
   def create
+    @article = Article.find(params[:id])
+    @user = current_user
+    @bookmark = Bookmark.new(user: @user, article: @article)
+    iff @review.save # Valid inputs
+      respond_to do |format| # Get input from html or js
+        format.html { redirect_to article_path(@article) }
+        format.js
+      end
+    else # Invalid inputs
+      respond_to do |format| # Get input from html or js
+        format.html { render "articles/show" }
+        format.js
+      end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614104537) do
+ActiveRecord::Schema.define(version: 20170614104950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,15 +66,6 @@ ActiveRecord::Schema.define(version: 20170614104537) do
     t.float    "source_score"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-  end
-
-  create_table "saved_articles", force: :cascade do |t|
-    t.integer  "article_id"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["article_id"], name: "index_saved_articles_on_article_id", using: :btree
-    t.index ["user_id"], name: "index_saved_articles_on_user_id", using: :btree
   end
 
   create_table "sources", force: :cascade do |t|
