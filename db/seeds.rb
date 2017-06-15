1761582ae0d6a56daf0804933ed374bfb6ecb0b0
Edward Stevinson <edward.stevinson@hotmail.com>
2017-06-15 23:11:24 +0100

    Seed UK sources with regex

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,49 +11,49 @@ right = -1.0
 # Create the DB entries of news sources
 # --- UK ---
 # Left
-# Source.create!(name: "New Yorker", source_score: left, url_base: "/(\S+)nyer\.cm(\S+)/")
-# Source.create!(name: "The Guardian", source_score: left, url_base: "/(\S+)www\.theguardian\.com(\S+)/")
-# Source.create!(name: "Al Jazeera", source_score: left, url_base: "/(\S+)aje\.io(\S+)|(\S+)www\.aljazeera\.com(\S+)/")
-# Source.create!(name: "HuffPost Politics", source_score: left, url_base: "/(\S+)huffp\.st(\S+)/")
-# Source.create!(name: "Vice", source_score: left, url_base: "/(\S+)www\.vice\.com(\S+)/")
-# Source.create!(name: "Morning Star", source_score: left, url_base: "/(\S+)www\.morningstaronline\.co\.uk(\S+)/")
-# # Centre left
-# Source.create!(name: "Bloomberg", source_score: c_left, url_base: "/(\S+)bloom\.bg(\S+)/")
-# Source.create!(name: "Independent", source_score: c_left, url_base: "/(\S+)www\.independent\.co\.uk(\S+)|(\S+)www\.indy100\.com(\S+)/")
-# Source.create!(name: "CNN", source_score: c_left, url_base: "/(\S+)cnn\.it(\S+)/")
-# Source.create!(name: "New York Times", source_score: c_left, url_base: "/(\S+)www\.nytimes\.com(\S+)/")
-# Source.create!(name: "NBC News", source_score: c_left, url_base: "/(\S+)nbcnews\.to(\S+)/")
-# # Centre right
-# Source.create!(name: "Sunday Times", source_score: c_right, url_base: "/(\S+)thetim\.es(\S+)/")
-# Source.create!(name: "Wall Street Journal", source_score: c_right, url_base: "/(\S+)wsj\.com(\S+)/")
-# Source.create!(name: "Economist", source_score: c_right, url_base: "/(\S+)econ\.st(\S+)/")
-# # Right
-# Source.create!(name: "Fox News", source_score: right, url_base: "/(\S+)foxnews\.com(\S+)|(\S+)fxn\.ws(\S+)/")
-# Source.create!(name: "Info Wars", source_score: right, url_base: "/(\S+)infowars\.com(\S+)/")
-# Source.create!(name: "Daily Mail", source_score: right, url_base: "/(\S+)dailym\.ai(\S+)/")
-# Source.create!(name: "The Sun", source_score: right, url_base: "/(\S+)thesun\.uk(\S+)/")
-# Source.create!(name: "Daily Express", source_score: right, url_base: "/(\S+)www\.express\.co\.uk(\S+)/")
-# Source.create!(name: "Daily Star", source_score: right, url_base: "/(\S+)www\.dailystar\.co\.uk(\S+)/")
+Source.create!(name: "New Yorker", source_score: left, url_base: "/(\S+)nyer\.cm(\S+)/")
+Source.create!(name: "The Guardian", source_score: left, url_base: "/(\S+)www\.theguardian\.com(\S+)/")
+Source.create!(name: "Al Jazeera", source_score: left, url_base: "/(\S+)aje\.io(\S+)|(\S+)www\.aljazeera\.com(\S+)/")
+Source.create!(name: "HuffPost Politics", source_score: left, url_base: "/(\S+)huffp\.st(\S+)/")
+Source.create!(name: "Vice", source_score: left, url_base: "/(\S+)www\.vice\.com(\S+)/")
+Source.create!(name: "Morning Star", source_score: left, url_base: "/(\S+)www\.morningstaronline\.co\.uk(\S+)/")
+# Centre left
+Source.create!(name: "Bloomberg", source_score: c_left, url_base: "/(\S+)bloom\.bg(\S+)/")
+Source.create!(name: "Independent", source_score: c_left, url_base: "/(\S+)www\.independent\.co\.uk(\S+)|(\S+)www\.indy100\.com(\S+)/")
+Source.create!(name: "CNN", source_score: c_left, url_base: "/(\S+)cnn\.it(\S+)/")
+Source.create!(name: "New York Times", source_score: c_left, url_base: "/(\S+)www\.nytimes\.com(\S+)/")
+Source.create!(name: "NBC News", source_score: c_left, url_base: "/(\S+)nbcnews\.to(\S+)/")
+# Centre right
+Source.create!(name: "Sunday Times", source_score: c_right, url_base: "/(\S+)thetim\.es(\S+)/")
+Source.create!(name: "Wall Street Journal", source_score: c_right, url_base: "/(\S+)wsj\.com(\S+)/")
+Source.create!(name: "Economist", source_score: c_right, url_base: "/(\S+)econ\.st(\S+)/")
+# Right
+Source.create!(name: "Fox News", source_score: right, url_base: "/(\S+)foxnews\.com(\S+)|(\S+)fxn\.ws(\S+)/")
+Source.create!(name: "Info Wars", source_score: right, url_base: "/(\S+)infowars\.com(\S+)/")
+Source.create!(name: "Daily Mail", source_score: right, url_base: "/(\S+)dailym\.ai(\S+)/")
+Source.create!(name: "The Sun", source_score: right, url_base: "/(\S+)thesun\.uk(\S+)/")
+Source.create!(name: "Daily Express", source_score: right, url_base: "/(\S+)www\.express\.co\.uk(\S+)/")
+Source.create!(name: "Daily Star", source_score: right, url_base: "/(\S+)www\.dailystar\.co\.uk(\S+)/")
 
 # --- France ---
 # Left
 # Source.create!(name: "Liberation", source_score: left, url_base: "//")
 # Source.create!(name: "L'Opinion", source_score: left, url_base: "//")
-Source.create!(name: "Le Nouvel Obs", source_score: left, url_base: "/nouvelobs\.com/")
-Source.create!(name: "Marianne", source_score: left, url_base: "/www\.marianne\.net/")
-Source.create!(name: "Charlie Hebdo", source_score: left, url_base: "/charliehebdo\.fr/")
-Source.create!(name: "Alter Eco", source_score: left, url_base: "/alternatives-economiques\.fr/")
-# Centre left
-Source.create!(name: "Le Monde", source_score: c_left, url_base: "/www\.lemonde\.fr/")
-Source.create!(name: "Courrier International", source_score: c_left, url_base: "/courrierinternational\.com/")
-# Centre right
-# Source.create!(name: "Les Echos", source_score: c_right, url_base: "//")
-Source.create!(name: "L'Express", source_score: c_right, url_base: "/lexpress\.fr/")
-# Right
-Source.create!(name: "Le Figaro", source_score: right, url_base: "/lefigaro\.fr/")
-Source.create!(name: "Le Point", source_score: right, url_base: "/lepoint\.fr/")
-Source.create!(name: "Le JDD", source_score: right, url_base: "/lejdd\.fr/")
-Source.create!(name: "Valeurs Actuelles", source_score: right, url_base: "/valeursactuelles\.com/")
+# Source.create!(name: "Le Nouvel Obs", source_score: left, url_base: "/nouvelobs\.com/")
+# Source.create!(name: "Marianne", source_score: left, url_base: "/www\.marianne\.net/")
+# Source.create!(name: "Charlie Hebdo", source_score: left, url_base: "/charliehebdo\.fr/")
+# Source.create!(name: "Alter Eco", source_score: left, url_base: "/alternatives-economiques\.fr/")
+# # Centre left
+# Source.create!(name: "Le Monde", source_score: c_left, url_base: "/www\.lemonde\.fr/")
+# Source.create!(name: "Courrier International", source_score: c_left, url_base: "/courrierinternational\.com/")
+# # Centre right
+# # Source.create!(name: "Les Echos", source_score: c_right, url_base: "//")
+# Source.create!(name: "L'Express", source_score: c_right, url_base: "/lexpress\.fr/")
+# # Right
+# Source.create!(name: "Le Figaro", source_score: right, url_base: "/lefigaro\.fr/")
+# Source.create!(name: "Le Point", source_score: right, url_base: "/lepoint\.fr/")
+# Source.create!(name: "Le JDD", source_score: right, url_base: "/lejdd\.fr/")
+# Source.create!(name: "Valeurs Actuelles", source_score: right, url_base: "/valeursactuelles\.com/")
 
 
 # Create the DB entries of news sources
