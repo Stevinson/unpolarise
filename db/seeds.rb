dc67a844fde9cbafcd43edbc7a69d3f644e6b1da
Amal Ketata <amal.ketata@edu.escpeurope.eu>
2017-06-15 12:08:36 +0100

    Added seed for FR newspapers

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,9 @@ left = 1.0
 c_left = 0.25
 c_right = -0.25
 right = -1.0
+
 # Create the DB entries of news sources
+# --- UK ---
 # Left
 Source.create!(name: "New Yorker", source_score: left, url_base: "/(\S+)nyer\.cm(\S+)/")
 Source.create!(name: "The Guardian", source_score: left, url_base: "/(\S+)www\.theguardian\.com(\S+)/")
@@ -32,6 +34,28 @@ Source.create!(name: "Daily Mail", source_score: right, url_base: "/(\S+)dailym\
 Source.create!(name: "The Sun", source_score: right, url_base: "/(\S+)thesun\.uk(\S+)/")
 Source.create!(name: "Daily Express", source_score: right, url_base: "/(\S+)www\.express\.co\.uk(\S+)/")
 Source.create!(name: "Daily Star", source_score: right, url_base: "/(\S+)www\.dailystar\.co\.uk(\S+)/")
+
+# --- France ---
+# Left
+# Source.create!(name: "Liberation", source_score: left, url_base: "//")
+# Source.create!(name: "L'Opinion", source_score: left, url_base: "//")
+Source.create!(name: "Le Nouvel Obs", source_score: left, url_base: "/nouvelobs\.com/")
+Source.create!(name: "Marianne", source_score: left, url_base: "/www\.marianne\.net/")
+Source.create!(name: "Charlie Hebdo", source_score: left, url_base: "/charliehebdo\.fr/")
+Source.create!(name: "Alter Eco", source_score: left, url_base: "/alternatives-economiques\.fr/")
+# Centre left
+Source.create!(name: "Le Monde", source_score: c_left, url_base: "/www\.lemonde\.fr/")
+Source.create!(name: "Courrier International", source_score: c_left, url_base: "/courrierinternational\.com/")
+# Centre right
+# Source.create!(name: "Les Echos", source_score: c_right, url_base: "//")
+Source.create!(name: "L'Express", source_score: c_right, url_base: "/lexpress\.fr/")
+# Right
+Source.create!(name: "Le Figaro", source_score: right, url_base: "/lefigaro\.fr/")
+Source.create!(name: "Le Point", source_score: right, url_base: "/lepoint\.fr/")
+Source.create!(name: "Le JDD", source_score: right, url_base: "/lejdd\.fr/")
+Source.create!(name: "Valeurs Actuelles", source_score: right, url_base: "/valeursactuelles\.com/")
+
+
 # Create the DB entries of news sources
 # Left
 # Page.create!(name: "Syriza", source_score: left, url_component: "syrizaofficial")
