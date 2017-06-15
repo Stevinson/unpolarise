3f84e249bf617c12249b08e9dcedd2a868cb9740
Edward Stevinson <edward.stevinson@hotmail.com>
2017-06-15 17:41:13 +0100

    Added name field from facebook

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -276,7 +276,7 @@ Devise.setup do |config|
   # config.omniauth_path_prefix = '/my_engine/users/auth'
   config.omniauth :facebook, ENV["FB_ID"], ENV["FB_SECRET"],
     scope: 'email',
-    info_fields: 'email, first_name, last_name',
+    info_fields: 'email, first_name, last_name, name', # added name
     image_size: 'square',  # 50x50, guaranteed ratio
     secure_image_url: true
 end
